dfab0c581129deba8409a6f247a170cdf64bbe12
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-03-20 15:45:04 +0100

    Add postgres query executor

--- back/managers/manager.js
+++ back/managers/manager.js
@@ -1,3 +1,5 @@
+const { Client } = require("pg"); // Postgres DB
+
 module.exports = class Manager {
   static clientParams = {
     user: process.env.DBUSER,
@@ -6,5 +8,27 @@ module.exports = class Manager {
     pswd: process.env.DBPSWD,
     port: process.env.DBPORT,
   };
-  static async queryExec() {}
+
+  static async executePostgresQuery(Model, query, params) {
+    let client = new Client(this.clientParams);
+    try {
+      client.connect();
+
+      let data = params
+        ? await client.query(query, params)
+        : await client.query(query);
+
+      return data.rows.map((e) => new Model(e));
+
+      //Returns undefined to diferenciate from empty array (no data)
+    } catch (error) {
+      console.log("Query Error: ", query, error);
+      return undefined;
+
+      //Close connection
+    } finally {
+      client.end();
+    }
+  }
+  static async executeMongoQuery(Model, query, params) {}
 };
